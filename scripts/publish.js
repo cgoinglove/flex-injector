8d47d7171f413ae7b09884cd5f677fbcd39452ae
cgoing <neo.cgoing@gmail.com>
2025-01-08 18:17:31 +0900

    fiexed: flex-inject latest 버전 사용

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,32 +1,52 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
+const path = require('path');
 
-const packageJsonPath = './package.json';
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const rootPath = process.cwd();
+const packagePath = path.join(rootPath, 'main');
+const packageJsonPath = path.join(packagePath, 'package.json');
+const readmeSourcePath = path.join(rootPath, 'README.md');
+const readmeDestinationPath = path.join(packagePath, 'README.md');
 
 const PUBLISH_NAME = 'flex-injector';
-
-try {
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-
-  const originalName = packageJson.name;
-
-  packageJson.name = PUBLISH_NAME;
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-  console.log('Updated package name to "flex-injector".');
-
-  console.log('Publishing package...');
-  execSync('npm publish', { stdio: 'inherit' });
-
-  packageJson.name = originalName;
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-  console.log(`Reverted package name to "${originalName}".`);
-} catch (error) {
-  console.error('Error during publish process:', error);
-  if (fs.existsSync(packageJsonPath)) {
+let ORIGIN_NAME = '';
+(async () => {
+  try {
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-    packageJson.name = 'root';
+    ORIGIN_NAME = packageJson.name;
+
+    packageJson.name = PUBLISH_NAME;
     fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-    console.log('Restored package name to "root" after error.');
+    console.log('Updated package name to "flex-injector".');
+
+    if (fs.existsSync(readmeSourcePath)) {
+      fs.copyFileSync(readmeSourcePath, readmeDestinationPath);
+      console.log('Copied README.md to main directory.');
+    } else {
+      console.warn('No README.md found in root directory.');
+    }
+
+    console.log('Publishing package...');
+
+    execSync('npm publish', { stdio: 'inherit', cwd: packagePath });
+    packageJson.name = ORIGIN_NAME;
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    console.log(`Reverted package name to "${ORIGIN_NAME}".`);
+  } catch (error) {
+    console.error('Error during publish process:', error);
+
+    if (fs.existsSync(packageJsonPath) && ORIGIN_NAME) {
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+      packageJson.name = ORIGIN_NAME;
+      fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+      console.log('Restored package name to "root" after error.');
+    }
+  } finally {
+    if (fs.existsSync(readmeDestinationPath)) {
+      fs.unlinkSync(readmeDestinationPath);
+      console.log('Removed README.md from main directory.');
+    }
   }
-  process.exit(1);
-}
+})();
